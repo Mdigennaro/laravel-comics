2234f6f341502ec2c6df54b21d63c91d93567dd5
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-21 16:52:29 +0100

    fine header top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.1/css/all.min.css" integrity="sha512-ioRJH7yXnyX+7fXTQEKPULWkMn3CqMcapK0NNtCN8q//sW7ZeVFcbMJ9RvX99TwDg6P8rAH2IqUSt2TLab4Xmw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/v4-shims.min.css" integrity="sha512-p++g4gkFY8DBqLItjIfuKJPFvTPqcg2FzOns2BNaltwoCOrXMqRIOqgWqWEvuqsj/3aVdgoEo2Y7X6SomTfUPA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
   <title>Home</title>
 </head>
 <body>
+
+  @include('partials.header')
 
   @yield('content')
   
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,40 @@
+<header>
+  <div class="header-top">
+    <div class="container">
+
+      <nav>
+        <ul>
+          <li>dc power™ visa®</li>
+          <li>additional dc sites<i class="fas fa-caret-down"></i></li>
+        </ul>
+      </nav>
+
+    </div>
+  </div>
+  <div class="header-bottom">
+    <div class="container">
+
+      <img src="" alt="">
+
+      <nav>
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </nav>
+
+      <div class="search-bar">
+        <input type="text" placeholder="search">
+      </div>
+
+    </div>
+  </div>
+</header>
